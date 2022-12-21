b5fc28e5bf722b318ce34969d86a3fa625d78fce
rufi3 <97358075+rufi3@users.noreply.github.com>
2022-12-21 11:19:17 +0100

    Add files via upload

--- javascript/script_kitchen.js
+++ javascript/script_kitchen.js
@@ -1,38 +1,3 @@
 let vh = window.innerHeight * 0.1
 
 document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-// Extended solution for hiding address bar:
-
-(function() {
-
-  var browser = window,
-      doc = browser.document;
-
-  // If there's a hash, or addEventListener is undefined, stop here
-  if ( !location.hash || !browser.addEventListener ) {
-
-    //set to 1
-    window.scrollTo( 0, 1 );
-    var scrollTop = 1,
-
-    //reset to 0 if needed
-    checkWindowBody = setInterval(function(){
-      if( doc.body ){
-        clearInterval( checkWindowBody );
-        scrollTop = "scrollTop" in doc.body ? doc.body.scrollTop : 1;
-        browser.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
-      }
-    }, 15 );
-
-    if (browser.addEventListener) {
-      browser.addEventListener("load", function(){
-        setTimeout(function(){
-          //reset to hide address
-          browser.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
-        }, 0);
-      }, false );
-    }
-  }
-
-})();
