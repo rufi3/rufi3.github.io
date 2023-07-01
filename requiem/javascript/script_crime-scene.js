a6a99e538ad48f4c255d447af1261f86ed897887
rufi3 <97358075+rufi3@users.noreply.github.com>
2023-07-01 22:55:25 +0200

    Add files via upload

---
+++ requiem/javascript/script_crime-scene.js
@@ -0,0 +1,31 @@
+let vh = window.innerHeight * 0.1
+
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+function openTestosterone() {
+  location.replace("testosterone.html")
+}
+
+function openWig() {
+  location.replace("wig.html")
+}
+
+function openPhone() {
+  location.replace("phone.html")
+}
+
+function openCable() {
+  location.replace("cable.html")
+}
+
+function openStaff() {
+  location.replace("staff.html")
+}
+
+function openPoster() {
+  location.replace("poster.html")
+}
+
+function openVictim() {
+  location.replace("victim.html")
+}
